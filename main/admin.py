2e0d1354f122ff63b9edce3ba924498ae5d76e2c
Jaewan, 再完 <wodhkszxc@gmail.com>
2022-10-11 21:06:02 +0900

    Models추가 및 내용 추가 etc

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import IT, ITAnswer, ITQuestion
+from .models import Humanity, HumanityAnswer, HumanityQuestion
+from .models import MediaContent, MediaContentsAnswer, MediaContentsQuestion
+from .models import Society, SocietyAnswer, SocietyQuestion
 
 # Register your models here.
 admin.site.register(IT)
 admin.site.register(ITQuestion)
 admin.site.register(ITAnswer)
+
+admin.site.register(Humanity)
+admin.site.register(HumanityAnswer)
+admin.site.register(HumanityQuestion)
+
+admin.site.register(Society)
+admin.site.register(SocietyAnswer)
+admin.site.register(SocietyQuestion)
+
+admin.site.register(MediaContent)
+admin.site.register(MediaContentsAnswer)
+admin.site.register(MediaContentsQuestion)
--- main/models.py
+++ main/models.py
@@ -23,3 +23,76 @@ class ITAnswer(models.Model):
     
     def __str__(self):
         return self.answer
+    
+#인문 model
+class Humanity(models.Model):
+    major = models.CharField(max_length=100)
+    count = models.IntegerField(default=0)
+    #image = models.ImageField('image') 학과별 이미지
+    
+    def __str__(self):
+        return self.major
+    
+class HumanityQuestion(models.Model):
+    number = models.IntegerField(unique=True)
+    question = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.number}. {self.question}'
+    
+class HumanityAnswer(models.Model):
+    major = models.ForeignKey(Humanity, on_delete=models.CASCADE)
+    question = models.ForeignKey(HumanityQuestion, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.answer
+    
+#사회 model
+class Society(models.Model):
+    major = models.CharField(max_length=100)
+    count = models.IntegerField(default=0)
+    #image = models.ImageField('image') 학과별 이미지
+
+    def __str__(self):
+        return self.major
+    
+class SocietyQuestion(models.Model):
+    number = models.IntegerField(unique=True)
+    question = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.number}. {self.question}'
+    
+class SocietyAnswer(models.Model):
+    major = models.ForeignKey(Society, on_delete=models.CASCADE)
+    question = models.ForeignKey(SocietyQuestion, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.answer
+    
+#미컨 model
+class MediaContent(models.Model):
+    major = models.CharField(max_length=100)
+    count = models.IntegerField(default=0)
+    #image = models.ImageField('image') 학과별 이미지
+    
+    def __str__(self):
+        return self.major
+    
+class MediaContentsQuestion(models.Model):
+    number = models.IntegerField(unique=True)
+    question = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return f'{self.number}. {self.question}'
+    
+class MediaContentsAnswer(models.Model):
+    major = models.ForeignKey(MediaContent, on_delete=models.CASCADE)
+    question = models.ForeignKey(MediaContentsQuestion, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.answer
